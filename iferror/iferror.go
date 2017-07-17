0afeba4b33aa293bf7f1fe830d3203f1acecea6c
pd29 <paolod29@gmail.com>
2017-07-17 10:23:04 +0200

    refactoring

--- iferror/iferror.go
+++ iferror/iferror.go
@@ -4,23 +4,34 @@ import (
 	"github.com/golang/glog"
 )
 
-func Log(err error) {
-	if err != nil {
-		glog.Error("ERROR:", err)
+func Log(res ...interface{}) interface{} {
+	for _, r := range res {
+		if err, ok := r.(error); ok {
+			if err != nil {
+				glog.Error("ERROR:", err)
+			}
+			return res
+		}
 	}
-}
-
-func Skip(res interface{}, err error) interface{} {
 	return res
 }
 
-func SkipLogging(res interface{}, err error) interface{} {
+func SkipIt(res interface{}, err error) interface{} {
+	return res
+}
+
+func SkipItLogging(res interface{}, err error) interface{} {
 	Log(err)
 	return res
 }
 
-func Fatal(err error) {
-	if err != nil {
-		glog.Fatal("ERROR:", err)
+func Fatal(res ...interface{}) {
+	for _, r := range res {
+		if err, ok := r.(error); ok {
+			if err != nil {
+				glog.Fatal("ERROR:", err)
+			}
+			return
+		}
 	}
 }
